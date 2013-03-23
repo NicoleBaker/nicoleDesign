c394d2d77ab69e8e93ab84d065c3878532c0d8d9
NicoleBaker <bakernicolej@gmail.com>
2013-03-23 10:48:00 -0700

    new portfolio and about page update

--- index.js
+++ index.js
@@ -9,3 +9,47 @@ $(".image-opener").click(function(event) {
 	$modal.find('h3').text(title);
 	$modal.children('.modal-body').html("<img src='" + image +"'>");
 });
+
+
+// portfolio see more open and closer
+$(".link-animation").click(function(event) {
+	var open   = $(this).attr('data-open');
+	var state  = $(this).attr('state');
+	var $arrow = $(this).find('.action-arrow');
+	
+	event.preventDefault();
+	if (state == 'closed') {
+		// open it
+		$(open).slideDown();
+		$(this).attr('state', 'opened');
+		flip($arrow, '180');
+	}else{
+		// close it
+		$(open).slideUp();
+		$(this).attr('state', 'closed');
+		flip($arrow, '360');
+	};
+});
+
+$(document).on('click', '.link-animation-closer', function() {
+	var parent_name = $(this).parents('.more-container').attr('id') + '-opener';
+	var $parent   = $('html').find('#' + parent_name);
+	var $see_more = $(this).parents('.more-container');
+	var $arrow    = $parent.find('.action-arrow');
+	
+	$see_more.slideUp();
+	$parent.attr('state', 'closed');
+	flip($arrow, '360');
+});
+
+function flip ($element, degrees) {
+	$element.css({
+	  '-webkit-transform' : 'rotate('+degrees+'deg)',
+	     '-moz-transform' : 'rotate('+degrees+'deg)',  
+	      '-ms-transform' : 'rotate('+degrees+'deg)',  
+	       '-o-transform' : 'rotate('+degrees+'deg)',  
+	          'transform' : 'rotate('+degrees+'deg)',  
+	               'zoom' : 1
+
+	    });
+}
